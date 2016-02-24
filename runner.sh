85f09879553f4068b674e8a717e832c3500a317c
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-24 12:49:30 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -f "/vpn/vpn_server.config" ]; then
+  cp /etc-start/vpn_server.config /vpn/vpn_server.config
+fi
+
 if [ ! -d "/var/log/vpnserver/security_log" ]; then
   mkdir -p /var/log/vpnserver/security_log
 fi
@@ -12,6 +16,7 @@ if [ ! -d "/var/log/vpnserver/server_log" ]; then
   mkdir -p /var/log/vpnserver/server_log
 fi
 
+chmod -R 777 /var/log/vpnserver/
 ln -s /var/log/vpnserver/*_log /usr/local/vpnserver/
 
 exec /usr/local/vpnserver/vpnserver execsvc
